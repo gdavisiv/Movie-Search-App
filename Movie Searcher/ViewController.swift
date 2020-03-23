b7e93f33e8b3ea2a4cbb8f45bb8c4a3a5da02607
gdavisiv <gdavisiv@gmail.com>
2020-03-23 13:50:30 -0400

    Initial Commit

---
+++ Movie Searcher/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Movie Searcher
+//
+//  Created by user163072 on 3/23/20.
+//  Copyright © 2020 George Davis IV. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
