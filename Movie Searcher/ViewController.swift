77f040a1e3c77cb756d35d131f7031d9e5263803
gdavisiv <gdavisiv@gmail.com>
2020-03-23 14:30:03 -0400

    Creating the variables and functions needed to search for movies

--- Movie Searcher/ViewController.swift
+++ Movie Searcher/ViewController.swift
@@ -8,13 +8,40 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+//UI
 
+//Network Request to get movie list
+
+//Tap a cell to see info about the movie
+
+//Custom Cell to show the movie
+
+
+class ViewController: UIViewController, UITextFieldDelegate, UITableViewDelegate, UITableViewDataSource {
+
+    //We'll need two outlets for a user interface a field and table
+    @IBOutlet var table: UITableView!
+    @IBOutlet var field: UITextField!
+        
     override func viewDidLoad() {
         super.viewDidLoad()
+        //Assign the delegate and dataSource
+        table.delegate = self
+        table.dataSource = self
+        field.delegate = self
         // Do any additional setup after loading the view.
     }
-
+    //Field
+    //This will capture the event when the user hits the return key
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        searchMovies()
+        return true
+    }
+    //This function will search for the movies
+    func searchMovies() {
+        
+    }
+    //create a function to search for the movies, and capture the eevnt when user hits the return key
 
 }
 
